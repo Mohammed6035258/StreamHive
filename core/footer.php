f4e6e3476d4fcd438dde65618e3f63263091ccf1
Mohammed6035258 <mohammednl2005@gmail.com>
2026-04-23 12:04:04 +0200

    Header/Footer Toevoeging
    Ik heb een header een een Footer toe gevoeg aan de index.php en wat stijling zodat het goed staat

---
+++ core/footer.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?> Mijn Website. Alle rechten voorbehouden.</p>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ core/index.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+
+<h2>Welkom op mijn website</h2>
+<p>Dit is de homepage.</p>
+
+<?php include 'footer.php'; ?>
